47f5b6d8d0835af5f93b6901ce3e197d49adbe4e
Albert_Benedek <99963622+AlbertBenedek1@users.noreply.github.com>
2024-01-02 00:11:51 +0200

    adatbázis összekötések, html kicserelése ejs-re, webshob oldal update

--- src/Locomotiv/index.js
+++ src/Locomotiv/index.js
@@ -32,7 +32,7 @@ app.get('/players', function (req, res) {
         await client.connect();
         const collection = client.db("Locomotiv").collection("Players");
         const players = await collection.find().toArray();
-  
+        
         res.send(players)
       } finally {
         // Ensures that the client will close when you finish/error
@@ -42,52 +42,48 @@ app.get('/players', function (req, res) {
     run().catch(console.dir);
 });
 
+
 //szerveroldal küldi a csapat és webshop html fájlokat is
 app.use(express.static(path.join(__dirname, 'public'))); //kiterjesztim ide is az express látókörét
 
-app.get('/csapat', function (req, res) {  //ez a /csapat egy interfésznek is felfogható, letakarja az elérési útvonalat vagy azt, hogy pl html fájl van koszolgálva
+/*app.get('/csapat', function (req, res) {  //ez a /csapat egy interfésznek is felfogható, letakarja az elérési útvonalat vagy azt, hogy pl html fájl van koszolgálva
   res.sendFile(path.join(__dirname, 'public', 'html', 'csapat.html')); // ha a http://localhost:3000/csapat linkre lépnek (most a navbarba a href ezekre a linkekre irányít) akkor ez html tartalom töltődik be, de a link nem kell ez legyen, azt én mondom meg, fel lehet fogni egy interfészként is
+});*/
+
+app.get('/csapat', function (req, res) {     
+  const filePath = path.join(__dirname, "public", "html", "csapat.ejs"); //tartalmazza az utat a speciális html fájlhoz
+  const client = getClient(); //tartalmazza a kapcsolatomat a MongoDB-vel
+  async function run() {
+    try {
+      await client.connect();                                               //csatlakozok a MongoDB-hez
+      const collection = client.db("Locomotiv").collection("Players");      //tartalmazza a collectionomet a MongoDB-ben
+      const players = await collection.find().toArray();                    //kihalássza a tartalmat a collectionbol
+      res.render(filePath, {players});                                      //megküldi az utat és a MongoDB-ben lévő collection tartalmat
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 });
 
 app.get('/webshop', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'html', 'webshop.html'));
+  const filePath = path.join(__dirname, "public", "html", "webshop.ejs"); 
+  const client = getClient(); 
+  async function run() {
+    try {
+      await client.connect();                                              
+      const collection = client.db("Locomotiv").collection("Shop");      
+      const products = await collection.find().toArray();                    
+      res.render(filePath, {products});                                      
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 });
 
 
-let fs = require("fs");
-
-app.set("view engine", "ejs");
-
-//jelen esetben ez a programrész azt feltételezi, hogy ez e views mappa létezek és van benne egy ilyen index.ejs
-/*app.get("/proba", (req, res) => {
-
-   fs.readFile("./characters.json", (err, file) => {
-    const characters = JSON.parse(file);
-    // (specialisHTML, adat) -> HTML
-    res.render("index2", { 
-        characters: characters,
-        // ...
-        // ...
-        // ...
-    });
-  });
-});*/
-
-app.get("/proba", (req, res) => {
-  fs.readFile("./characters.json", (err, file) => {
-    const characters = JSON.parse(file);
-    // Teljes elérési út a webshop.ejs fájlhoz
-    const filePath = path.join(__dirname, "public", "html", "webshop.ejs");
-    // (specialisHTML, adat) -> HTML
-    res.render(filePath, { 
-        characters: characters,
-        // ...
-        // ...
-        // ...
-    });
-  });
-});
-
-
-
+//let fs = require("fs");
+//app.set("view engine", "ejs");
 app.listen(3000);
